50285328ec4e52a6eff4f792aa00c976e40fc713
Samuel Russell <srussell@uber.com>
2018-02-02 13:19:12 -0500

    Add hist common

--- hist_common.py
+++ hist_common.py
@@ -15,11 +15,11 @@ if len(sys.argv) > 1:
 def _print_hist(with_rate=True):
     global seen, lprint, t0
     sortseen = sorted(seen.items(), key=operator.itemgetter(1), reverse=True)
-    print '------------------------------------------------------------------'
+    print '------------------------------------------ {} unique total'.format(len(sortseen))
     if with_rate:
-        print '  cnt  rate'
+        print '  cnt  rate  thing'
     else:
-        print '  cnt'
+        print '  cnt thing'
     tdelt = seconds_between(t0, get_now())
     for x in sortseen[0:n]:
         tot = int(x[1])
@@ -32,7 +32,7 @@ def _print_hist(with_rate=True):
 for line in quick_ingest_line():
     line = line.rstrip().lstrip()
     seen[line] = seen.get(line, 0) + 1
-    if seconds_between(lprint, get_now()) > 3:
+    if  seconds_between(lprint, get_now()) > 3:
         lprint = get_now()
         _print_hist()
 
