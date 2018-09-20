a245cea15ecc744564cd294ff05820020abe8762
Samuel Russell <srussell@uber.com>
2018-09-20 09:02:05 -0400

    Add more tensorflow examples

---
+++ examples/tensorflow/hello.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python
+
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
+import tensorflow as tf
+
+node1 = tf.constant(3.0, tf.float32)
+node2 = tf.constant(4.0)
+print (node1, node2)
+
+sess = tf.Session()
+print sess.run([node1, node2])
+
+node3 = tf.add(node1, node2)
+print "node3:", node3
+print "sess.run: ", sess.run(node3)
+
+print '\n\nsetting up a variable graph\n'
+
+W = tf.Variable([.3], tf.float32)
+b = tf.Variable([-.3], tf.float32)
+x = tf.placeholder(tf.float32)
+linear_model = W * x + b
+
+init = tf.global_variables_initializer()
+sess.run(init)
+
+print(sess.run(linear_model, {x:[1,2,3,4]}))
+
+
+print '\n\nrunning loss function\n'
+
+y = tf.placeholder(tf.float32)
+squared_deltas = tf.square(linear_model - y)
+loss = tf.reduce_sum(squared_deltas)
+print(sess.run(loss, {x:[1,2,3,4], y:[0,-1,-2,-3]}))
+
+print '\n\nguessed perfect w/b, now loss is\n'
+
+fixW = tf.assign(W, [-1.])
+fixb = tf.assign(b, [1.])
+sess.run([fixW, fixb])
+print(sess.run(loss, {x:[1,2,3,4], y:[0,-1,-2,-3]}))
