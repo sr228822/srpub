e08c1541c5dfc803b2ae5f4e8666c6e08f0a62c9
Samuel Russell <srussell@uber.com>
2018-10-11 16:22:56 -0400

    Update go examples

--- examples/go/copy_struct.go
+++ examples/go/copy_struct.go
@@ -5,12 +5,18 @@ import (
 )
 
 type Foo struct {
+	// primitives work great
 	Count int
 	Name string
+
+	// pointers and complex structures aren't deep copies tho
+	StringPointer *string
+	MyMap map[string]string
 }
 
 func main() {
-	a := &Foo{Count: 1, Name:"hello"}
+	neat := "neat"
+	a := &Foo{Count: 1, Name:"hello", StringPointer: &neat, MyMap: map[string]string{"one": "1111", "two": "2222"}}
 
 	// copy a to b
 	b := *a
@@ -21,4 +27,11 @@ func main() {
 	// print both
     fmt.Println(a)
 	fmt.Println(b)
+
+	// updating complex fields in A updates b
+	*a.StringPointer = "updated"
+	a.MyMap["three"] = "33333"
+	fmt.Printf("these fields are shared between a and b\n")
+	fmt.Println(b.MyMap)
+	fmt.Println(*b.StringPointer)
 }
