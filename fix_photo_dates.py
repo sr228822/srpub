b610f646dee86f4a4820a84b94d19d05333b9f71
Samuel Russell <samrussell@fb.com>
2022-08-06 17:13:23 -0400

    Add fix_photo_dates.py

---
+++ fix_photo_dates.py
@@ -0,0 +1,31 @@
+#!/usr/env/python
+
+import os
+from srutils import *
+
+for path in sys.argv[1:]:
+    for root, dir, files in os.walk(path):
+        day = 1
+        hour = 1
+        print(root)
+        for f in sorted(files):
+            path = os.path.join(root, f)
+            if not path.endswith("jpg"):
+                continue
+            folder = path.split("/")[0]
+            start = folder.split(" ")[1].split("-")
+            month = start[0]
+            year = start[1]
+            if len(year) == 2:
+                year = f"19{year}"
+            assert (int(year) > 1980 and int(year) < 2010)
+            full_date = f"{year}:{month}:{day:02} {hour}:01:01-04:00"
+            hour += 1
+            if hour > 22:
+                hour = 1
+                day += 1
+            print(path, full_date)
+            print(cmd(f'exiftool "-CreateDate={full_date}" "{path}"'))
+            os.remove(f"{path}_original")
+
+
