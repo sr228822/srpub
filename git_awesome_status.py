3e20ee6ae1ffeb611fad16b5d502fed8b14d2d38
Samuel Russell <samrussell@fb.com>
2023-09-16 18:04:57 -0800

    cleanup git-awesome-status to use main

--- git_awesome_status.py
+++ git_awesome_status.py
@@ -12,21 +12,6 @@ from operator import itemgetter
 from srutils import *
 
 ###########################################################
-#     Parse args
-###########################################################
-parser = argparse.ArgumentParser()
-parser.add_argument(
-    "--archive", type=str, required=False, help="Archive the specified branch"
-)
-parser.add_argument(
-    "--master", action="store_true", help="Force remote branch to master"
-)
-parser.add_argument(
-    "--all", action="store_true", help="Show all missing commits from master"
-)
-args = parser.parse_args()
-
-###########################################################
 #     Helper classes & functions
 ###########################################################
 class Commit:
@@ -153,232 +138,250 @@ def fb_alternate():
     return "master"
 
 
-###########################################################
-#     Init and argv stuff
-###########################################################
-showall = args.all
-rows, cols = get_term_size()
-root = cmd("git rev-parse --show-toplevel")
-repo_name = root.split("/")[-1]
+if __name__ == "__main__":
+    ###########################################################
+    #     Parse args
+    ###########################################################
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--archive", type=str, required=False, help="Archive the specified branch"
+    )
+    parser.add_argument(
+        "--master", action="store_true", help="Force remote branch to master"
+    )
+    parser.add_argument(
+        "--all", action="store_true", help="Show all missing commits from master"
+    )
+    args = parser.parse_args()
 
-# Read cache file
-home = os.path.expanduser("~")
-# TODO templatize by project
-cachef = os.path.join(home, f".git_awesome_status_{repo_name}")
-cache = {}
+    ###########################################################
+    #     Init and argv stuff
+    ###########################################################
+    showall = args.all
+    rows, cols = get_term_size()
+    root = cmd("git rev-parse --show-toplevel")
+    repo_name = root.split("/")[-1]
 
-if os.path.isfile(cachef):
-    with open(cachef, "r") as f:
-        dat = f.read()
-        cache = json.loads(dat)
+    # Read cache file
+    home = os.path.expanduser("~")
+    # TODO templatize by project
+    cachef = os.path.join(home, f".git_awesome_status_{repo_name}")
+    cache = {}
 
-archived_branches = cache.get("archived_branches", [])
+    if os.path.isfile(cachef):
+        with open(cachef, "r") as f:
+            dat = f.read()
+            cache = json.loads(dat)
 
-###########################################################
-#     Archive branches
-###########################################################
-if args.archive:
-    archived_branches.append(args.archive)
-    cache["archived_branches"] = archived_branches
-    with open(cachef, "w") as f:
-        f.write(json.dumps(cache))
-    sys.exit(0)
+    archived_branches = cache.get("archived_branches", [])
 
-###########################################################
-#     Fetch info about the branch and remote branch
-###########################################################
+    ###########################################################
+    #     Archive branches
+    ###########################################################
+    if args.archive:
+        archived_branches.append(args.archive)
+        cache["archived_branches"] = archived_branches
+        with open(cachef, "w") as f:
+            f.write(json.dumps(cache))
+        sys.exit(0)
 
-# Print a new line..... seems to help windows reset its coloring
-print("")
+    ###########################################################
+    #     Fetch info about the branch and remote branch
+    ###########################################################
 
-status = cmd("git status -bs").rstrip().split("\n")
-if not status or len(status) == 0 or not status[0]:
-    print(red_str("Not a git repo"))
-    sys.exit(1)
-branchline = status[0] + " "
-m = re.search(r"\.\.\.(.*?) ", branchline)
-if m:
-    fb = m.group(1)
-    branchline = branchline.replace("...", " ")
-    current_branch = branchline.split()[1]
-else:
-    current_branch = branchline.split()[1]
-    fb = fb_alternate()
-if fb is None:
-    raise Exception("Failed to detect forward-branch")
+    # Print a new line..... seems to help windows reset its coloring
+    print("")
 
-master = args.master
-if master:
-    fb = "origin/master"
-
-###########################################################
-#     Fetch the log info about local HEAD and remote branch
-###########################################################
-
-second_try = False
-tot_n = 100
-origin_n = 200
-
-while True:
-    tot = fetch_commits_for_branch("", tot_n)
-    totsha = [c.sha for c in tot]
-    tottitle = [c.title for c in tot]
-
-    if not fb:
-        # not a remote-tracking branch
-        for c in tot[0:6]:
-            show_sha(c.sha)
+    status = cmd("git status -bs").rstrip().split("\n")
+    if not status or len(status) == 0 or not status[0]:
+        print(red_str("Not a git repo"))
         sys.exit(1)
-
-    origin = fetch_commits_for_branch(fb, origin_n)
-    originsha = [c.sha for c in origin]
-    origintitle = [c.title for c in origin]
-
-    # How many commits in each category
-    made = [c for c in tot[0:50] if c.title not in origintitle]
-    made_merged = [
-        c for c in tot[0:50] if c.title in origintitle and c.sha not in originsha
-    ]
-    missing = [
-        c
-        for c in origin[0 : (tot_n - len(made))]
-        if c.sha not in totsha and c.title not in tottitle
-    ]
-    common = [c for c in origin if c.sha in totsha and c.title in tottitle]
-
-    if len(made) + len(made_merged) < 15:
-        break
+    branchline = status[0] + " "
+    m = re.search(r"\.\.\.(.*?) ", branchline)
+    if m:
+        fb = m.group(1)
+        branchline = branchline.replace("...", " ")
+        current_branch = branchline.split()[1]
     else:
-        if second_try:
-            # something is wierd
+        current_branch = branchline.split()[1]
+        fb = fb_alternate()
+    if fb is None:
+        raise Exception("Failed to detect forward-branch")
+
+    master = args.master
+    if master:
+        fb = "origin/master"
+
+    ###########################################################
+    #     Fetch the log info about local HEAD and remote branch
+    ###########################################################
+
+    second_try = False
+    tot_n = 100
+    origin_n = 200
+
+    while True:
+        tot = fetch_commits_for_branch("", tot_n)
+        totsha = [c.sha for c in tot]
+        tottitle = [c.title for c in tot]
+
+        if not fb:
+            # not a remote-tracking branch
+            for c in tot[0:6]:
+                show_sha(c.sha)
+            sys.exit(1)
+
+        origin = fetch_commits_for_branch(fb, origin_n)
+        originsha = [c.sha for c in origin]
+        origintitle = [c.title for c in origin]
+
+        # How many commits in each category
+        made = [c for c in tot[0:50] if c.title not in origintitle]
+        made_merged = [
+            c for c in tot[0:50] if c.title in origintitle and c.sha not in originsha
+        ]
+        missing = [
+            c
+            for c in origin[0 : (tot_n - len(made))]
+            if c.sha not in totsha and c.title not in tottitle
+        ]
+        common = [c for c in origin if c.sha in totsha and c.title in tottitle]
+
+        if len(made) + len(made_merged) < 15:
             break
         else:
-            tot_n = 1000
-            origin_n = 1200
-            second_try = True
+            if second_try:
+                # something is wierd
+                break
+            else:
+                tot_n = 1000
+                origin_n = 1200
+                second_try = True
 
-## Uncomment this line to run a fetch before very call, slower but shows a more accurate state #######
-# cmd('git fetch')
+    ## Uncomment this line to run a fetch before very call, slower but shows a more accurate state #######
+    # cmd('git fetch')
 
-###########################################################
-#     print branch info
-###########################################################
-other_branches = cmd("git branch").strip().split("\n")
-other_branches = [x.strip() for x in other_branches if not x.startswith("*")]
-branches_n = len(other_branches)
-if args.all:
-    archived_branches = []
-other_branches = [x for x in other_branches if x not in archived_branches]
-archived_n = len(other_branches) - branches_n
+    ###########################################################
+    #     print branch info
+    ###########################################################
+    other_branches = cmd("git branch").strip().split("\n")
+    other_branches = [x.strip() for x in other_branches if not x.startswith("*")]
+    branches_n = len(other_branches)
+    if args.all:
+        archived_branches = []
+    other_branches = [x for x in other_branches if x not in archived_branches]
+    archived_n = len(other_branches) - branches_n
 
-# max_len = max([len(x) for x in other_branches]) + 10
-max_len = 50
-branch_cols = min(3, int(cols / max_len))
+    # max_len = max([len(x) for x in other_branches]) + 10
+    max_len = 50
+    branch_cols = min(3, int(cols / max_len))
 
-print
-print("*" * (cols - 10))
-if current_branch == "HEAD":
-    print(magenta_str(bold_str("%30s" % "~~ HEAD detached ~~")))
-else:
-    print(blue_str(bold_str("%30s" % current_branch)))
-branch_data = []
-for branch in other_branches:
-    try:
-        stats = cmd(
-            'git log -n 1 --pretty=format:"%ci %cr" ' + branch + " -- | head -n 1"
-        ).split(" ")
-        dt = datetime.datetime.strptime(stats[0] + " " + stats[1], "%Y-%m-%d %H:%M:%S")
-        age = stats[3] + " " + stats[4]
-    except:
-        dt = datetime.datetime.now()
-        age = ""
-    branch_data.append((branch, dt, age))
-
-for idx, dat in enumerate(sorted(branch_data, key=itemgetter(1), reverse=True)):
-    branch, dt, age = dat
-    if "minutes" in age or "hours" in age:
-        age = ""
-    print("%30s " % branch + grey_str("%-10s" % age), end="")
-    if (idx + 1) % branch_cols == 0 or idx == len(branch_data) - 1:
-        print("")
-if archived_branches:
-    print(grey_str("%30s" % f"{len(archived_branches)} archived branches"))
-print("*" * (cols - 10))
-
-###########################################################
-#     print local status diffs
-###########################################################
-untracked = []
-for l in status[1:]:
-    if "??" in l:
-        untracked.append(l)
+    print
+    print("*" * (cols - 10))
+    if current_branch == "HEAD":
+        print(magenta_str(bold_str("%30s" % "~~ HEAD detached ~~")))
     else:
-        print(magenta_str(l))
+        print(blue_str(bold_str("%30s" % current_branch)))
+    branch_data = []
+    for branch in other_branches:
+        try:
+            stats = cmd(
+                'git log -n 1 --pretty=format:"%ci %cr" ' + branch + " -- | head -n 1"
+            ).split(" ")
+            dt = datetime.datetime.strptime(
+                stats[0] + " " + stats[1], "%Y-%m-%d %H:%M:%S"
+            )
+            age = stats[3] + " " + stats[4]
+        except:
+            dt = datetime.datetime.now()
+            age = ""
+        branch_data.append((branch, dt, age))
 
-if len(untracked) > 3:
-    print(red_str(str(len(untracked)) + " Untracked files ??"))
-else:
-    for u in untracked:
-        print(red_str(u))
-print("")
+    for idx, dat in enumerate(sorted(branch_data, key=itemgetter(1), reverse=True)):
+        branch, dt, age = dat
+        if "minutes" in age or "hours" in age:
+            age = ""
+        print("%30s " % branch + grey_str("%-10s" % age), end="")
+        if (idx + 1) % branch_cols == 0 or idx == len(branch_data) - 1:
+            print("")
+    if archived_branches:
+        print(grey_str("%30s" % f"{len(archived_branches)} archived branches"))
+    print("*" * (cols - 10))
 
-###########################################################
-#     print commits cherry picked on top
-###########################################################
-done = 0
-cp_but_merged = 0
+    ###########################################################
+    #     print local status diffs
+    ###########################################################
+    untracked = []
+    for l in status[1:]:
+        if "??" in l:
+            untracked.append(l)
+        else:
+            print(magenta_str(l))
 
-if len(made) + len(made_merged) > 20:
-    # something is wierd
-    if len(origin) == 0:
-        # we don't generally get here, since we fallback to master
-        print(grey_str("    ********** (no origin) ************"))
+    if len(untracked) > 3:
+        print(red_str(str(len(untracked)) + " Untracked files ??"))
     else:
-        print(red_str("\nUnable to determine alignment with remote\n"))
-    show_shas(totsha[0], totsha[8])
-    sys.exit(1)
+        for u in untracked:
+            print(red_str(u))
+    print("")
 
-for c in tot[0:50]:
-    if c in made:
-        done += 1
-        show_sha(c.sha)
-    if c in made_merged:
-        # Cherry picked and merged
-        done += 1
-        cp_but_merged += 1
-        show_sha_magenta(c.sha)
+    ###########################################################
+    #     print commits cherry picked on top
+    ###########################################################
+    done = 0
+    cp_but_merged = 0
 
-# usually happens if there is no origin
-if len(common) == 0:
-    sys.exit(1)
+    if len(made) + len(made_merged) > 20:
+        # something is wierd
+        if len(origin) == 0:
+            # we don't generally get here, since we fallback to master
+            print(grey_str("    ********** (no origin) ************"))
+        else:
+            print(red_str("\nUnable to determine alignment with remote\n"))
+        show_shas(totsha[0], totsha[8])
+        sys.exit(1)
 
-if fb == "master":
-    print(grey_str("    ********** " + fb + " ************"))
-else:
-    print(blue_str("    ********** " + fb + " ************"))
+    for c in tot[0:50]:
+        if c in made:
+            done += 1
+            show_sha(c.sha)
+        if c in made_merged:
+            # Cherry picked and merged
+            done += 1
+            cp_but_merged += 1
+            show_sha_magenta(c.sha)
 
-###########################################################
-#     print commits in origin missing from HEAD'
-###########################################################
-if len(missing) > 5 and not showall:
-    print(grey_str("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"))
-    print(grey_str("   " + str(len(missing)) + " missing commits"))
-    print(grey_str("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"))
-    done += 2
-elif len(missing) > 0:
-    for c in missing:
-        show_sha_grey(c.sha)
-        done += 1
+    # usually happens if there is no origin
+    if len(common) == 0:
+        sys.exit(1)
 
-###########################################################
-#     print merged commits in origin and HEAD
-###########################################################
+    if fb == "master":
+        print(grey_str("    ********** " + fb + " ************"))
+    else:
+        print(blue_str("    ********** " + fb + " ************"))
 
-left = min(max(1, 10 - done), len(common) - 1)
-originz = show_shas(common[0].sha, common[left].sha, nmax=left)
+    ###########################################################
+    #     print commits in origin missing from HEAD'
+    ###########################################################
+    if len(missing) > 5 and not showall:
+        print(grey_str("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"))
+        print(grey_str("   " + str(len(missing)) + " missing commits"))
+        print(grey_str("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"))
+        done += 2
+    elif len(missing) > 0:
+        for c in missing:
+            show_sha_grey(c.sha)
+            done += 1
 
-###########################################################
-#     print my last merged commit if not in the above
-###########################################################
-if "Samuel Russell" not in originz and "Sam Russell" not in originz:
-    show_my_most_recent(fb)
+    ###########################################################
+    #     print merged commits in origin and HEAD
+    ###########################################################
+
+    left = min(max(1, 10 - done), len(common) - 1)
+    originz = show_shas(common[0].sha, common[left].sha, nmax=left)
+
+    ###########################################################
+    #     print my last merged commit if not in the above
+    ###########################################################
+    if "Samuel Russell" not in originz and "Sam Russell" not in originz:
+        show_my_most_recent(fb)
